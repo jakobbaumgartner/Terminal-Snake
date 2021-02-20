88d514c7d40bcbf37d3671a69f9df971e01e5b61
JB <jakob.baumgartner@hotmail.com>
2021-02-20 14:58:09 +0100

    move up and down

--- snake.py
+++ snake.py
@@ -3,13 +3,13 @@ class Snake:
 
     # location = [25,25] # current location of the head of the snake
 
-    direction = 2 # direction of moving of the snake
+    direction = 0 # direction of moving of the snake
     # 0 - up
     # 1 - down
     # 2 - left
     # 3 - right
 
-    body = [[25,25,0],[25,26,1],[25,27,1],[25,28,1]]
+    body = [[25,25,0],[26,25,1],[27,25,1],[28,25,1]]
     #[y,x,element_type]
     # element type:
     # 0 - head
@@ -19,9 +19,23 @@ class Snake:
     def SnakeMove (self):
        # snake moves in the direction that was chosen
 
+    # snake move function:
+    # - remove the last element 
+    # - change the type of the first element to BODY 
+    # - add an element type HEAD to the beginning ( in the chosen DIRECTION) 
+
+
        self.body.pop() #remove the last element
 
        (self.body[0])[2] = 1
+
+       if self.direction == 0: 
+           ycoord = ((self.body[0])[1] - 1) # when moving up, we decrease y - coord by one
+           xcoord = (self.body[0])[1] # x - coord is the same as elements before it
+
+       if self.direction == 1: 
+           ycoord = ((self.body[0])[1] + 1) # when moving down, we increase y - coord by one
+           xcoord = (self.body[0])[1] # x - coord is the same as elements before it
 
        if self.direction == 2:
            ycoord = (self.body[0])[0] # moving left, y - coord is the same as the element before
